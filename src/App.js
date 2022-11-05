8725cdc82b3fc87fadb07c9c50c9df55b2d23796
Anozieluciana <anoziechiderasilverlin@gmail.com>
2022-11-05 10:53:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from "./Component/Header/Header"
+import Profile from './Component/Profile/Profile'
+// import Foter from './Component/Foter/Foter'
+import MyWishes from './Component/Wish/MyWishes'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import GlobalState from './Global'
+import Sent from './Component/Wish/SentWishes'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <GlobalState/>
+      <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Profile/>}/>
+      <Route path='/my' element={<MyWishes/>}/>
+      <Route path='/mys' element={<Sent/>}/>
+    </Routes>
+    {/* <Foter/> */}
+    </BrowserRouter>
+      {/* </GlobalState> */}
+    </>
+    
+
+  )
 }
 
-export default App;
+export default App
